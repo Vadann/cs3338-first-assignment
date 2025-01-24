a2992bd3e571509eba7632cfb4fffc794436cfc2
Vadan <haykvadan@gmail.com>
2025-01-24 11:08:34 -0800

    Added old assignment

---
+++ lab1.py
@@ -0,0 +1,29 @@
+str1 = "Hello "
+str2 = "World"
+str3 = str1 + str2
+print(str3)
+
+# Area of rectangle  = W * L
+
+width = 10
+length = 5
+area_of_rectangle = width * length
+print(area_of_rectangle)
+
+# CELSIUS CONVERSION: I use input to make printing several examples easier. Just re-run program and put different value.
+
+celsius = float(input("Enter celsius: "))
+fahrenheit = celsius * (9 / 5) + 32
+
+print(fahrenheit)
+
+
+# A CS student does not need to take upper division GE courses because they are met by major.
+
+# No CS4961 can only be taken in fall and then the following CS4962 has to be taken in the following spring.
+
+# No ,the degree planner is automated and may not be the most accurate, so it should be manually adjusted if needed.
+
+# Yes this is true and important to make sure you can graduate on time.
+
+
